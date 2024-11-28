0f222e26bba4a383cbcc7a0d3f0ae746904f6a4b
Biswanath Das <128900883+dbiswanath@users.noreply.github.com>
2024-11-28 18:46:48 +0530

    Add files via upload

---
+++ FlowTT/js/MainPage.js
@@ -0,0 +1,26 @@
+var swiper = new swiper(". swiper", {
+  slidesPerView: 1,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 1,
+      slidesPerGroup: 7,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
